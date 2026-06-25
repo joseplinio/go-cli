b334a9912eed5c4bb70600addc91c56165308597
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-06-25 15:52:01 -0300

    (Add) message's cli and the welcome message
    I know, I know this code is not too good yet, but this's not its
    propuse, I just wanted to write some go code to work out  my "go mind"
    ?. I don't know, but it's working (so far lamon)

--- main.go
+++ main.go
@@ -3,13 +3,36 @@ package main
 import (
 	"fmt"
 	"github.com/damienbutt/figlet"
+	"time"
 )
 
 func main() {
-	text, err := figlet.Text("go cli")
+	// NOTE: Showing up that the cli is working
+	welcome_message := welcomeMessage()
+	cli_message := toShowInitMessage()
+
+	fmt.Printf("%v -- %v\n", cli_message, welcome_message)
+}
+func toShowInitMessage() string {
+	text,
+		err := figlet.Text("go cli")
 	if err != nil {
 		panic(err)
 	}
+	return text
+}
+func welcomeMessage() string {
+	real_time := time.Now()
+	hours, _, _ := real_time.Clock()
 
-	fmt.Println(text)
+	if hours >= 06 && hours < 12 {
+		return "Good morning."
+	} else if hours >= 12 && hours < 18 {
+		return "Good afternoon."
+	} else if hours >= 18 && hours < 23 {
+		return "Good nigth."
+	} else {
+		return "Please go to sleep bro you need!"
+	}
+
 }
